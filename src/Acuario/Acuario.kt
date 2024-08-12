e2db7f662b08ca168a749bbcd65a88c1cb510a2e
Deyson Flores <deyson.flores@tecsup.edu.pe>
2024-08-12 17:05:14 -0500

    LAB2

---
+++ src/Acuario/Acuario.kt
@@ -0,0 +1,30 @@
+package Acuario
+
+class Acuario(var largo: Int = 100, var ancho: Int = 20, var alto: Int = 40) {
+
+    init {
+        println("inicializando acuario")
+    }
+
+    var volumen: Int
+        get() = ancho * alto * largo / 1000  // 1000 cm^3 = 1 l
+        set(valor) {
+            alto = (valor * 1000) / (ancho * largo)
+        }
+
+    constructor(numeroDePeces: Int) : this() {
+        // 2,000 cm^3 por pez + espacio extra para que no se derrame
+
+        val tanque = numeroDePeces * 2000 * 1.1
+        alto = (tanque / (largo * ancho)).toInt()
+    }
+
+    fun imprimirTamano() {
+        println("Ancho: $ancho cm " +
+                "Largo: $largo cm " +
+                "Alto: $alto cm "
+        )
+        // 1 l = 1000 cm^3
+        println("Volumen: $volumen l")
+    }
+}
